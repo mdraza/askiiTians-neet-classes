3f230f3b91bf5c96b5e6ad9c207ae1513df01c8a
mdraza <raza.uidev@gmail.com>
2020-03-30 14:07:29 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*---------- Video Play Button ------*/
+$('#play-video').on('click', function(e){
+    e.preventDefault();
+    $('#video-overlay').addClass('open');
+    $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+  });
+  
+  $('.video-overlay, .video-overlay-close').on('click', function(e){
+    e.preventDefault();
+    close_video();
+  });
+  
+  $(document).keyup(function(e){
+    if(e.keyCode === 27) { close_video(); }
+  });
+  
+  function close_video() {
+    $('.video-overlay.open').removeClass('open').find('iframe').remove();
+  };
+  /*---------- Testimonial Carousal ---------*/
+  $('#owl-carousel1').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
