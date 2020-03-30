8c769daafd90e8631e5ebfde4b3b921eaff1fe74
mdraza <raza.uidev@gmail.com>
2020-03-30 17:30:25 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -5,18 +5,6 @@ $('#play-video').on('click', function(e){
     $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
   });
   
-  $('.video-overlay, .video-overlay-close').on('click', function(e){
-    e.preventDefault();
-    close_video();
-  });
-  
-  $(document).keyup(function(e){
-    if(e.keyCode === 27) { close_video(); }
-  });
-  
-  function close_video() {
-    $('.video-overlay.open').removeClass('open').find('iframe').remove();
-  };
   /*---------- Testimonial Carousal ---------*/
   $('#owl-carousel1').owlCarousel({
     loop: true,
@@ -36,4 +24,6 @@ $('#play-video').on('click', function(e){
             items: 1
         }
     }
-})
+});
+  /*---------- AOS Animation Init ---------*/
+//   AOS.init();
